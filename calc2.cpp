c99b42b713a64fe83abbb542ab6d5046f1d2156e
Firojahamad <137470698+Firojahamad@users.noreply.github.com>
2023-06-23 11:14:15 +0530

    Add files via upload

---
+++ calc2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+	return a+b;
+}
+int main()
+{
+	int m,n;
+	cin>>m>>n;
+	cout<<"the sum of the m and n is :"<<sum(m,n);
+	
+	return 0;
+}
